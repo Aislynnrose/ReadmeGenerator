d5d7f46f86046609ebd5e02dd5b834d5d7b33ab0
Aislynn Uribe <aislynn.rose@gmail.com>
2021-06-25 20:43:24 -0600

    init repo and created format and questions for generator

---
+++ index.js
@@ -0,0 +1,94 @@
+
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [ 
+  {
+    type: 'input',
+    message: 'What is your project title?',
+    name: 'title',
+  },
+  {
+    type: 'list',
+    message: 'What is your license?',
+    name: 'license',
+    choices: [
+      'Apache 2.0',
+      'Perl',
+      'MIT',
+      'IBM',
+      'Eclipse',
+    ]
+  },
+  {
+    type: 'input',
+    message: 'What is your usage purpose?',
+    name: 'usage',
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "What are the Please enter contribution guidelines for your project.",
+},
+{
+    type: "input",
+    name: "test",
+    message: "Waht are the test instructions for your project.",
+},
+  {
+    type: 'input',
+    message: 'What is your GitHub URL?',
+    name: 'GitHubURL',
+  },
+  {
+    type: 'input',
+    message: 'What is your deployed URL?',
+    name: 'deployedURL',
+  },
+  {
+    type: 'input',
+    message: 'paste your screenshot or gif preview URL',
+    name: 'preview',
+  },
+  {
+  type: 'checkbox',
+  name: 'technologies',
+  message: 'What technologies did you use?',
+  choices: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'jQuery',
+    'Node.js',
+    'Bootstrap',
+    'moment.js',
+    'REACT',
+    'vue.js',
+    'C#',
+    'c++',
+]}
+]
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+  err ? console.error(err) : console.log("New README file created successfully!")
+  );
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
+  .then(response => {
+    const fileText = generateMarkdown(response);
+    const fileName = response.title + "-readme.md";
+    writeToFile(fileName, fileText);
+});
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,70 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ('');
+  }
+  else {
+    return(`[![License: ${license}](https://img.shields.io/badge/License-${license}-0298c3.svg)]`)
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (!license) {
+    return ('');
+  }
+    else {
+      return(`(https://opensource.org/licenses/${license})`)
+    }
+  }
+
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) {
+    return ('');
+  }
+  else {
+    return(`
+    ## License
+    ${renderLicenseBadge(license)}
+    ${renderLicenseLink(license)}
+    `
+    );
+}
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `#** ${data.title} **
+  
+  ${renderLicenseSection(data.license)}
+
+  
+  ## Table of Contents 
+  * [Usage](#Usage)
+  * [Contributions](#Contributions)
+  * [Technologies](#technologies)
+
+  ## Usage
+  * ${data.usage}
+
+  ## Contributions
+  * ${data.contributions}
+
+  ## _Deployed URL_: ${data.deployedURL}
+  
+  ## _GitHub URL_: ${data.GitHubURL}
+
+  ## Preview: ![Screenshot](${data.preview})
+
+  # Technologies Used
+  *${data.technologies}
+
+`;
+}
+
+module.exports = generateMarkdown;
